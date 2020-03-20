8c4ebe06d160de698c758b8d68f7cf605b4d60a5
Ben Sarmiento <me@bensarmiento.com>
2020-03-20 14:23:24 +0800

    Setup cron job in github actions

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const options = {
-	appId: process.env.FB_APP_ID,
-	appSecret: process.env.FP_APP_SECRET
-};
-const groupId = process.env.FB_GROUP_ID;
+console.log('Hello world')
 
-const FB = require('fb'),
-	fb = new FB.Facebook(options);
+// const options = {
+// 	appId: process.env.FB_APP_ID,
+// 	appSecret: process.env.FP_APP_SECRET
+// };
+// const groupId = process.env.FB_GROUP_ID;
 
-fb.api(
-	`/${groupId}/feed`,
-	(response) => {
-		if (response && !response.error) {
-			response.data.map(post => {
-				console.log(`${post.message}`)
-				console.log(`${post.updated_time}`)
-				console.log(`${post.id}`)
-			})
-		}
-	}
-);
+// const FB = require('fb'),
+// 	fb = new FB.Facebook(options);
+
+// fb.api(
+// 	`/${groupId}/feed`,
+// 	(response) => {
+// 		if (response && !response.error) {
+// 			response.data.map(post => {
+// 				console.log(`${post.message}`)
+// 				console.log(`${post.updated_time}`)
+// 				console.log(`${post.id}`)
+// 			})
+// 		}
+// 	}
+// );
